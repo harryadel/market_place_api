a1bf99cb1a6c4b724083049fb33dc7e1e4f19f61
Harry Adel <harryadelb@gmail.com>
2020-07-25 21:46:31 +0200

    Setup tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     # we are going to list our resources here
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
+      resources :tokens, only: [:create]
     end
   end
 end
